5d23298a30faf7a1d17d6ecb9d0f21bd977aa825
Kalysbek <xyz@DESKTOP-C66P4HG.>
2024-11-05 02:37:16 +0000

    .

--- config/settings.py
+++ config/settings.py
@@ -113,7 +113,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [(REDIS_URL, 0)],  
+            "hosts": [(os.environ.get('REDIS_HOST', 'redis'), 6379)],  
         },
     },
 }
