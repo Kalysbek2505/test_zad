ae97f9d8c1843192202dc7ec03fbd1d02f4ad8fc
Kalysbek <xyz@DESKTOP-C66P4HG.>
2024-11-05 00:52:16 +0000

    .

--- config/settings.py
+++ config/settings.py
@@ -83,11 +83,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'NAME': os.environ('DB_NAME'),
+        'USER': os.environ('DB_USER'),
+        'PASSWORD': os.environ('DB_PASSWORD'),
+        'HOST': os.environ('DB_HOST'),
+        'PORT': os.environ('DB_PORT'),
     }
 }
 
